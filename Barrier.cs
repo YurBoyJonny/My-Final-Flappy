c02edda4c3a6420c83877d85f9fd606dbc82bf49
cheujona853 <cheujona853@2260I-L42-74>
2022-06-23 10:59:03 -0400

    Add project files.

---
+++ Barrier.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace My_Final_Flappy
+{
+    class Barrier
+    {
+        private Texture2D _texture;
+        private Rectangle _location;
+        private Vector2 _speed; // _speed.X is horizontal speed, _speed.Y is vertical speed
+        public Barrier(Texture2D texture, int x, int y)
+        {
+            _texture = texture;
+            _location = new Rectangle(x, y, 100, 100);
+            _speed = new Vector2();
+        }
+        public float HSpeed
+        {
+            get { return _speed.X; }
+            set { _speed.X = value; }
+        }
+        public float VSpeed
+        {
+            get { return _speed.Y; }
+            set { _speed.Y = value; }
+        }
+        private void Move()
+        {
+            _location.X += (int)_speed.X;
+            _location.Y += (int)_speed.Y;
+        }
+        public void Update()
+        {
+            Move();
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _location, Color.White);
+        }
+        public bool Collide(Rectangle item)
+        {
+            return _location.Intersects(item);
+        }
+        public void UndoMove()
+        {
+            _location.X -= (int)_speed.X;
+            _location.Y -= (int)_speed.Y;
+        }
+        public void Grow()
+        {
+            _location.Width += 1;
+            _location.Height += 1;
+        }
+    }
+}
